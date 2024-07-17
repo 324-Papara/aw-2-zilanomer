1624590e5d7701d682d1aa3f5809b4aec2506ea5
zilan.omer <zilanomer477@gmail.com>
2024-07-17 22:47:56 +0300

    Update database configurations for local environment

---
+++ Para.Api/Startup.cs
@@ -0,0 +1,61 @@
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Para.Data.Context;
+using Para.Data.UnitOfWork;
+
+namespace Para.Api;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+    
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+    }
+    
+    
+    public void ConfigureServices(IServiceCollection services)
+    {
+               
+        services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+            options.JsonSerializerOptions.WriteIndented = true;
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Para.Api", Version = "v1" });
+        });
+
+        var connectionStringSql = Configuration.GetConnectionString("MsSqlConnection");
+        services.AddDbContext<ParaSqlDbContext>(options =>
+            options.UseSqlServer(connectionStringSql, sqlOptions =>
+                sqlOptions.MigrationsAssembly("Para.Api"))); // Migration assembly'yi belirtiyoruz
+
+        var connectionStringPostgre = Configuration.GetConnectionString("PostgresSqlConnection");
+        services.AddDbContext<ParaPostgreDbContext>(options => options.UseNpgsql(connectionStringPostgre));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Para.Api v1"));
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
